fba980949d4364be726526c7e582aa005a4df67e
marscode <39918899+marscode@users.noreply.github.com>
2019-06-17 01:17:56 +0800

    Add files via upload

---
+++ my_bookstore/bookstore/static/js/checkbox.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.checkAll').on('click', function () {
+      $(this).closest('table').find('tbody :checkbox')
+        .prop('checked', this.checked)
+        .closest('tr').toggleClass('selected', this.checked);
+    });
+  
+    $('tbody :checkbox').on('click', function () {
+      $(this).closest('tr').toggleClass('selected', this.checked);
+  
+      $(this).closest('table').find('.checkAll').prop('checked', ($(this).closest('table').find('tbody :checkbox:checked').length == $(this).closest('table').find('tbody :checkbox').length));
+    });
+  });
